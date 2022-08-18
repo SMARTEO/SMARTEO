caf660bdf882f1a535d55a27c3544bc52b236ade
AlphaConseils <alpha@alphamada.com>
2022-08-18 12:52:14 +0200

    FEAT[smt_sale] : Function to update all record and views

--- smt_sale/models/sale_order.py
+++ smt_sale/models/sale_order.py
@@ -51,32 +51,32 @@ class SaleOrder(models.Model):
                     order.order_line.mapped('purchase_price_subtotal'))
 
     def update_all_margin(self):
-        if not all(self._ids):
-            for line in self.env["sale.order"].search([]):
-                for record in line.order_line:
-                    record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
-                    record.margin_percent = (record.purchase_price * record.product_uom_qty) and record.margin / (
-                            record.purchase_price * record.product_uom_qty)
-                line.margin = sum(line.order_line.mapped('margin'))
+        # if not all(self._ids):
+        for line in self.env["sale.order"].search([]):
+            for record in line.order_line:
+                record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
+                record.margin_percent = (record.purchase_price * record.product_uom_qty) and record.margin / (
+                        record.purchase_price * record.product_uom_qty)
+            line.margin = sum(line.order_line.mapped('margin'))
 
-                line.margin_percent = sum(line.order_line.mapped('purchase_price_subtotal')) and line.margin / sum(
-                    line.order_line.mapped('purchase_price_subtotal'))
-        else:
-            self.env["sale.order.line"].flush(['margin'])
-            # On batch records recomputation (e.g. at install), compute the margins
-            # with a single read_group query for better performance.
-            # This isn't done in an onchange environment because (part of) the data
-            # may not be stored in database (new records or unsaved modifications).
-            grouped_order_lines_data = self.env['sale.order.line'].read_group(
-                [
-                    ('order_id', 'in', self.ids),
-                ], ['margin', 'order_id'], ['order_id'])
-            mapped_data = {m['order_id'][0]: m['margin'] for m in grouped_order_lines_data}
-            for order in self:
-                for record in order.order_line:
-                    record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
-                    record.margin_percent = (record.purchase_price * record.product_uom_qty) and record.margin / (
-                            record.purchase_price * record.product_uom_qty)
-                order.margin = mapped_data.get(order.id, 0.0)
-                order.margin_percent = sum(order.order_line.mapped('purchase_price_subtotal')) and order.margin / sum(
-                    order.order_line.mapped('purchase_price_subtotal'))
+            line.margin_percent = sum(line.order_line.mapped('purchase_price_subtotal')) and line.margin / sum(
+                line.order_line.mapped('purchase_price_subtotal'))
+        # else:
+        #     self.env["sale.order.line"].flush(['margin'])
+        #     # On batch records recomputation (e.g. at install), compute the margins
+        #     # with a single read_group query for better performance.
+        #     # This isn't done in an onchange environment because (part of) the data
+        #     # may not be stored in database (new records or unsaved modifications).
+        #     grouped_order_lines_data = self.env['sale.order.line'].read_group(
+        #         [
+        #             ('order_id', 'in', self.ids),
+        #         ], ['margin', 'order_id'], ['order_id'])
+        #     mapped_data = {m['order_id'][0]: m['margin'] for m in grouped_order_lines_data}
+        #     for order in self:
+        #         for record in order.order_line:
+        #             record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
+        #             record.margin_percent = (record.purchase_price * record.product_uom_qty) and record.margin / (
+        #                     record.purchase_price * record.product_uom_qty)
+        #         order.margin = mapped_data.get(order.id, 0.0)
+        #         order.margin_percent = sum(order.order_line.mapped('purchase_price_subtotal')) and order.margin / sum(
+        #             order.order_line.mapped('purchase_price_subtotal'))
