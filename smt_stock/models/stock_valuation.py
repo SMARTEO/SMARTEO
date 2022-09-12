dd23a65622409ff78df212ab9ca6a0b12c9030f8
rivo2302 <rivo2302@gmail.com>
2022-09-12 13:12:58 +0300

    [FEAT] smt_stock : Valorisation stock

---
+++ smt_stock/models/stock_valuation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class StockValuationLayerInherit(models.Model):
+    _inherit = "stock.valuation.layer"
+    categ_id = fields.Many2one(
+        "product.category", related="product_id.categ_id", store=True
+    )
+    defaul_code = fields.Char(
+        related="product_id.default_code", store=True, string="Reference article"
+    )
+
+    standard_price = fields.Float(
+        related="product_id.standard_price", store=True, string="Prix unitaire"
+    )
