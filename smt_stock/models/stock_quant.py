cd59d36ac3d4f9b1ceba85ff65d4c702f0f01ff9
AlphaConseils <rivo2302@gmail.com>
2022-09-21 13:08:50 +0300

    [FEAT] smt_stock : Filtre par categorie d'article

--- smt_stock/models/stock_quant.py
+++ smt_stock/models/stock_quant.py
@@ -11,6 +11,14 @@ class StockQuant(models.Model):
         store=True,
         group_operator="avg",
     )
+
+    category_id = fields.Many2one(
+        "product.category",
+        related="product_id.categ_id",
+        string="Catégorie",
+        readonly=True,
+        store=True,
+    )
     values = fields.Float(compute="_compute_values", string="Valeur total", store=True)
 
     @api.depends("inventory_quantity_auto_apply", "standard_price")
