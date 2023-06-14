7ef9ddc0d735b38526c2d535a7fdbaf435ebe9a4
michael <michael@nexources.com>
2023-06-14 14:24:54 +0300

    [FEAt]add new view and filter planning issu

--- smt_stock/controllers/controllers.py
+++ smt_stock/controllers/controllers.py
@@ -1,21 +1,63 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+#-*- coding: utf-8 -*-
+from collections import deque
+import io
+import json
 
+from odoo import http, _
+from odoo.http import content_disposition, request
+from odoo.tools import ustr, osutil
+from odoo.tools.misc import xlsxwriter
 
-# class SmtStock(http.Controller):
-#     @http.route('/smt_stock/smt_stock', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class SmtStock(http.Controller):
 
-#     @http.route('/smt_stock/smt_stock/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('smt_stock.listing', {
-#             'root': '/smt_stock/smt_stock',
-#             'objects': http.request.env['smt_stock.smt_stock'].search([]),
-#         })
+    # @http.route('/export', type='http', auth='user')
+    # def export_handler(self, **kw):
+    #     model = request.env['stock.picking']
+    #     response = model.export_data_planning_issu()
+    #     return response(environ=request.httprequest.environ)
+    def _get_display_name(self, record, field_name):
+        return getattr(record, field_name).display_name if field_name.endswith("_id") else str(getattr(record, field_name))
+    @http.route('/stock_picking/export/xlsx', type='http', auth='user')
+    def export_xlsx_handler(self, ids):
+        output = io.BytesIO()
+        ids = [int(id) for id in ids.strip('][').split(', ')]
+        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+        worksheet = workbook.add_worksheet(_('Stock Picking'))
+        header_bold = workbook.add_format({'bold': True, 'pattern': 1, 'bg_color': '#AAAAAA'})
+        header_plain = workbook.add_format({'pattern': 1, 'bg_color': '#AAAAAA'})
+        bold = workbook.add_format({'bold': True})
+        headers = request.env['stock.picking']._fields.keys()
+        for c, header in enumerate(headers):
+            worksheet.write(0, c, header, header_bold)
+        records = request.env['stock.picking'].browse(ids)
+        l = 1
+        for record in records:
+            for c, field_name in enumerate(headers):
+                worksheet.write(l, c, self._get_display_name(record, field_name), header_plain)
+            l += 1
+        workbook.close()
+        xlsx_data = output.getvalue()
+        filename = osutil.clean_filename(_("Raport %(title)s (%(model_name)s)", title=_('Problèmes de planning'), model_name="stock_picking"))
+        response = request.make_response(xlsx_data,
+                                         headers=[('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),
+                                                  ('Content-Disposition', content_disposition(filename + '.xlsx'))],
+                                         )
 
-#     @http.route('/smt_stock/smt_stock/objects/<model("smt_stock.smt_stock"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('smt_stock.object', {
-#             'object': obj
-#         })
+        return response
+    #@http.route('/smt_stock/smt_stock', auth='public')
+    # def index(self, **kw):
+    #     return "Hello, world"
+    #
+    # @http.route('/smt_stock/smt_stock/objects', auth='public')
+    # def list(self, **kw):
+    #     return http.request.render('smt_stock.listing', {
+    #         'root': '/smt_stock/smt_stock',
+    #         'objects': http.request.env['smt_stock.smt_stock'].search([]),
+    #     })
+    #
+    # @http.route('/smt_stock/smt_stock/objects/<model("smt_stock.smt_stock"):obj>', auth='public')
+    # def object(self, obj, **kw):
+    #     return http.request.render('smt_stock.object', {
+    #         'object': obj
+    #     })
+
--- smt_stock/models/stock_picking.py
+++ smt_stock/models/stock_picking.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, fields, models
+from odoo import _, api, fields, models, http
+
+from odoo.exceptions import AccessError
+import csv
+import os
+# from odoo.tools.misc import xlsxwriter
+import base64
+import re
+import io
+from openpyxl import Workbook
+from odoo.http import request
+from werkzeug.wrappers import Response
+from odoo.http import Controller
+import json
 
 class PickingType(models.Model):
     _inherit = "stock.picking.type"
@@ -15,6 +28,113 @@ class PickingType(models.Model):
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+
+    # def export_data(self, fields_to_export):
+    #     if not self.env.is_admin():
+    #         raise AccessError(_("Only administrators are allowed to export mail message"))
+    #
+    #     return super(StockPicking, self).export_data(fields_to_export)
+
+
+    # def export_data_planning_issu(self):
+    #
+    #     records = self.search([])
+    #     fieldnames = ['name', 'location_id', 'location_dest_id', 'partner_id']
+    #
+    #     folder_path = '/home/michael/Downloads'
+    #     filename = 'export_data.csv'
+    #
+    #     if not os.path.exists(folder_path):
+    #         os.makedirs(folder_path)
+    #
+    #     file_path = os.path.join(folder_path, filename)
+    #
+    #     with open(file_path, 'w', newline='') as csvfile:
+    #         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #
+    #         writer.writeheader()
+    #         for record in records:
+    #             writer.writerow({
+    #                 'name': record.name,
+    #                 'location_id': record.location_id,
+    #                 'location_dest_id': record.location_dest_id,
+    #                 'partner_id': record.partner_id
+    #             })
+    #
+    #     return True
+
+    # def export_data_planning_issu(self):
+    #     # Récupérer les enregistrements à exporter
+    #     records = self.search([])
+    #
+    #     # Créer un nouveau classeur Excel
+    #     workbook = Workbook()
+    #
+    #     # Créer une feuille de calcul
+    #     sheet = workbook.active
+    #
+    #     # Écrire les en-têtes de colonnes
+    #     sheet.append(['name', 'location_id', 'location_dest_id', 'partner_id'])
+    #
+    #     # Parcourir les enregistrements et écrire chaque ligne
+    #     for record in records:
+    #         sheet.append([record.name, record.location_id.name, record.location_dest_id.name, record.partner_id.name])
+    #
+    #     # Définir le chemin d'accès et le nom du fichier de sortie
+    #     filename = 'export_data.xlsx'
+    #
+    #     # Enregistrer le classeur Excel dans le fichier XLSX
+    #     workbook.save(filename)
+    #
+    #     # Faire quelque chose avec le fichier exporté, par exemple l'envoyer en tant que réponse HTTP
+    #
+    #     return True
+
+
+
+
+
+    def export_data_planning_issu(self):
+
+        records = self.search([])
+
+        workbook = Workbook()
+
+        sheet = workbook.active
+
+        headers = ['Champ 1', 'Champ 2', 'Champ 3']
+        sheet.append(headers)
+
+        for record in records:
+            data = [record.name, record.name, record.name]
+            sheet.append(data)
+        file_stream = io.BytesIO()
+        workbook.save(file_stream)
+        file_stream.seek(0)
+
+        file_content = base64.b64encode(file_stream.read()).decode('utf-8')
+
+
+        filename = 'export_data.xlsx'
+
+        # Renvoyer le fichier Excel en tant que réponse HTTP pour le téléchargement automatique
+        response = Response(
+            file_content,
+            headers=[
+                ('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),
+                ('Content-Disposition', 'attachment; filename=%s' % filename),
+            ]
+        )
+        return response
+
+    def exp_button(self):
+        # data = self.export_data(self._fields.keys())
+        return {
+            "type": "ir.actions.act_url",
+            "url": f"/stock_picking/export/xlsx?ids={self.ids}",
+        }
+
+
 
     # @api.model
     # def default_get(self, fields):
