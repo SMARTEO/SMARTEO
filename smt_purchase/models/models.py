7b9ed8bbf706bbea53a6bfb4c4a53d6eb5302dd9
michael <michael@nexources.com>
2023-07-27 10:14:05 +0300

    [FIX] # fix bug purchase bill

--- smt_purchase/models/models.py
+++ smt_purchase/models/models.py
@@ -16,5 +16,3 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
-
-domain="partner_id and [('company_id', '=', company_id), ('partner_id.commercial_partner_id', '=', commercial_partner_id)] or [('company_id', '=', company_id)]"
