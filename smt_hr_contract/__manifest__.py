4ec5d0d761193361cc987f3b5fcb04494b7904b1
michael <michael@nexources.com>
2023-09-18 18:14:23 +0300

    [FIX]#2523

---
+++ smt_hr_contract/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "smt_hr_contract",
+    "author": "My Company",
+    "website": "http://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+
+    "depends": ["base", "hr_contract", "hr_payroll"],
+
+    "data": [
+        "views/hr_contract_view.xml",
+    ],
+    'license': 'LGPL-3',
+}
