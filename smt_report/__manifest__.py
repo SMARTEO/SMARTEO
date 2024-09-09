62263c9cee4d3638a9670c4209de2f9b5cca3cb0
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 <michael@nexources>
2024-09-09 18:10:19 +0300

    [MERGE] ticket#1932_p

--- smt_report/__manifest__.py
+++ smt_report/__manifest__.py
@@ -15,9 +15,7 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
-    'depends': ['base','account','sale','sale_management'],
-
-  
+    'depends': ['base','account','sale','sale_management'],  
     'data': [
         'report/report_invoice_inherit.xml',
         'report/report_sale_order_inherit.xml',
