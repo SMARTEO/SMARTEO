fb3b4ce3df2029fd2e6cc733c20738632bde4486
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 <michael@nexources>
2024-09-18 10:16:32 +0300

    [ADD] smarteo invoice

--- smt_report/__manifest__.py
+++ smt_report/__manifest__.py
@@ -15,12 +15,11 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
-    'depends': ['base','account','sale','sale_management'],
-
-  
+    'depends': ['base','account','sale','sale_management'],  
     'data': [
         'report/report_invoice_inherit.xml',
         'report/report_sale_order_inherit.xml',
         'report/action_inherit.xml',
+        'report/layout_boxed.xml',
     ],
 }
