11e4ec10ebe5dc964bba1dd88d8c741deff9534a
moria-nexources <moria-nexources>
2025-08-24 08:54:10 +0300

    raise error if no lost stage found

--- smt_crm/models/crm_lead.py
+++ smt_crm/models/crm_lead.py
@@ -1,6 +1,7 @@
 #-*- cofing: utf-8-*-
 import logging
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class CrmLead(models.Model):
@@ -8,6 +9,8 @@ class CrmLead(models.Model):
 
     def _move_opportunities_to_lost_stage(self):
         lost_stage_id = self.env['crm.stage'].search([('is_lost', '=', True)], limit=1)
+        if not lost_stage_id:
+            raise ValidationError(_('No lost stage, this action can not be carried on !'))
         lost_opportunities = self.env['crm.lead'].search([('type', '=', 'opportunity'), ('active', '=', False), ('probability', '=', 0), ('stage_id', '!=', lost_stage_id.id)])
         for lost_opportunity in lost_opportunities:
             lost_opportunity.write({'stage_id' : lost_stage_id.id})
