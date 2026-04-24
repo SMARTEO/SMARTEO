6ba962485fca775cacfc12585348b92290000411
Andy Warren <warren@nexources.com>
2026-04-24 11:56:09 +0300

    [REF]

--- access_restriction_by_ip/models/res_users.py
+++ access_restriction_by_ip/models/res_users.py
@@ -29,3 +29,4 @@ class ResUsersInherit(models.Model):
     allowed_ip_ids = fields.One2many('allowed.ips', 'user_ip_id',
                                      string='IP Address',
                                      help="Allowed ip addresses for the user.")
+    allowed_ips = fields.Many2many('allowed.ips')
