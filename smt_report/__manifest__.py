71cb560b4f8be94ac4d302e2ba3c57ea76f0f124
Andy Warren <warren@nexources.com>
2026-06-01 13:24:53 +0300

    [IMP] smt_report: total padding

--- smt_report/__manifest__.py
+++ smt_report/__manifest__.py
@@ -1,17 +1,11 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "smt_report",
-    'summary': "Smarteo Reports — invoice and sale order layout overrides",
-    'author': "NEXOURCES",
-    'website': "https://www.nexources.com",
-    'category': 'Accounting',
-    'version': '19.0.1.0.0',
-    'depends': ['base', 'account', 'sale', 'sale_management'],
-    'data': [
-        # 'report/report_invoice_inherit.xml',
-        # 'report/report_sale_order_inherit.xml',
-        # 'report/action_inherit.xml',
-        # 'report/layout_boxed.xml',
-    ],
-    'license': 'LGPL-3',
+    "name": "SMARTEO - Report",
+    "summary": "Smarteo Reports — invoice and sale order layout overrides",
+    "author": "NEXOURCES",
+    "website": "https://www.nexources.com",
+    "category": "Accounting",
+    "version": "19.0.1.1.1",
+    "depends": ["account"],
+    "data": ["report/report_invoice_document.xml"],
+    "license": "LGPL-3",
 }
