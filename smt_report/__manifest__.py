a932bfc641befe1e5b8f4cfe2a4ae2b498cbd7d3
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 <michael@nexources>
2026-06-30 12:44:39 +0300

    [ADD] smt_report:improvement on pdfs

--- smt_report/__manifest__.py
+++ smt_report/__manifest__.py
@@ -5,7 +5,13 @@
     "website": "https://www.nexources.com",
     "category": "Accounting",
     "version": "19.0.1.1.1",
-    "depends": ["account"],
-    "data": ["report/report_invoice_document.xml"],
+    "depends": ["account", "sale"],
+    "data": [
+        "report/report_invoice_document.xml",
+        "report/report_invoice_inherit.xml",
+        "report/action_inherit.xml",
+        "report/layout_boxed.xml",
+        "report/report_sale_order_inherit.xml",
+    ],
     "license": "LGPL-3",
 }
